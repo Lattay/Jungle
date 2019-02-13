c16ce6ceffeb39f70bf979efb4293bc336725579
Theo Cavignac <theo.cavignac@ecl16.ec-lyon.fr>
2019-02-13 12:08:54 +0100

    Use fixed index names for arrays and tidy up stack operations

--- src/family.c
+++ src/family.c
@@ -8,17 +8,17 @@ void init_families_full_random(population* pop){
         nspec = (int) map1(RAND(), 0.5, 1.0) * INIT_FAM_NUMBER / VAR_COEF;
     }
     float ag, fr, sc, vi, norm;
-    for(int i = 0; i < INIT_FAM_NUMBER; i++){
-        pop->fam[i].species = (int) map(i, 0, INIT_FAM_NUMBER, 0, nspec);
+    for(int f = 0; f < INIT_FAM_NUMBER; f++){
+        pop->fam[f].species = (int) map(f, 0, INIT_FAM_NUMBER, 0, nspec);
 
         ag = RAND(); fr = RAND(); sc = RAND(); vi = RAND();
         norm = sqrt(ag*ag + fr*fr + sc*sc + vi*vi);
 
-        pop->fam[i].aggresiveness = ag / norm;
-        pop->fam[i].fertility = fr / norm;
-        pop->fam[i].sociability = sc / norm;
-        pop->fam[i].vitality = vi / norm;
-        pop->fam[i].ressources = 0;
+        pop->fam[f].aggresiveness = ag / norm;
+        pop->fam[f].fertility = fr / norm;
+        pop->fam[f].sociability = sc / norm;
+        pop->fam[f].vitality = vi / norm;
+        pop->fam[f].ressources = 0;
     }
 }
 
@@ -26,17 +26,17 @@ void init_families_from_species(population* pop, int nspec, family_t* spec,
                                 int fam_spec_map[INIT_FAM_NUMBER]){
     int sp;
     float ag, fr, sc, vi, norm;
-    for(int i = 0; i < INIT_FAM_NUMBER; i++){
-        sp = fam_spec_map[i];
+    for(int f = 0; f < INIT_FAM_NUMBER; f++){
+        sp = fam_spec_map[f];
         ag = spec[sp].aggresiveness * map1(RAND(), 0.5, 1.5);
         fr = spec[sp].fertility * map1(RAND(), 0.5, 1.5);
         sc = spec[sp].sociability * map1(RAND(), 0.5, 1.5);
         vi = spec[sp].vitality * map1(RAND(), 0.5, 1.5);
         norm = sqrt(ag*ag + fr*fr + sc*sc + vi*vi);
-        pop->fam[i].aggresiveness = ag / norm;
-        pop->fam[i].fertility = fr / norm;
-        pop->fam[i].sociability = sc / norm;
-        pop->fam[i].vitality = vi / norm;
-        pop->fam[i].ressources = 0;
+        pop->fam[f].aggresiveness = ag / norm;
+        pop->fam[f].fertility = fr / norm;
+        pop->fam[f].sociability = sc / norm;
+        pop->fam[f].vitality = vi / norm;
+        pop->fam[f].ressources = 0;
     }
 }
--- src/individual.c
+++ src/individual.c
@@ -2,26 +2,26 @@
 
 void init_pop(population* pop){
 
-    int k;
-    for(int i = 0; i < INIT_FAM_NUMBER; i++){
+    int i;
+    for(int k = 0; k < INIT_FAM_NUMBER; k++){
 
-        for(int j = 0; j < INIT_FAM_SIZE; j++){
-            k = INIT_FAM_SIZE * i + j;
-            pop->dead[k + INIT_POP] = -1;
-            pop->alive[k] = k;
-            pop->indiv[k].direction = RAND();
-            pop->indiv[k].speed = RAND();
-            pop->indiv[k].vitality = pop->fam[i].vitality * (0.5 + RAND() / 2);
-            pop->indiv[k].family = i;
+        for(int h = 0; h < INIT_FAM_SIZE; h++){
+            k = INIT_FAM_SIZE * k + h;
+            pop->dead[i + INIT_POP] = -1;
+            pop->alive[i] = i;
+            pop->indiv[i].direction = RAND();
+            pop->indiv[i].speed = RAND();
+            pop->indiv[i].vitality = pop->fam[k].vitality * (0.5 + RAND() / 2);
+            pop->indiv[i].family = i;
         }
     }
-    for(int i = INIT_POP; i < MAX_POP; i++){
-        pop->alive[i] = -1;
-        pop->dead[i-INIT_POP] = i;
+    for(int k = INIT_POP; k < MAX_POP; k++){
+        pop->alive[k] = -1;
+        pop->dead[k-INIT_POP] = k;
     }
 }
 
-
+/* Stack will be kept tidy
 static void sort_dead_stack(population* pop){
     int first_free = 0;
     int last_occupied = MAX_POP;
@@ -45,27 +45,66 @@ static void sort_alive_stack(population* pop){
     }
     pop->atop = first_free;
 }
+*/
+
+// Kill given the position in the indiv array (useful)
+void kill_indiv(population* pop, int i){
+    int a = 0;  
+    while(pop->alive[a] != i) a++;
+    pop->alive[a] = pop->alive[pop->atop--];
+    pop->dead[pop->dtop++] = i;
+}
+// Kill given the position in the stack (faster)
+int kill_alive(population* pop, int a){
+    int i = pop->alive[a];
+    pop->alive[a] = pop->alive[pop->atop--];
+    pop->dead[pop->dtop++] = i;
+    return i;
+}
+// Kill the top of the stack (fastest) (useless ?)
+int kill_top(population* pop){
+    int i = pop->alive[pop->atop--];
+    pop->dead[pop->dtop++] = i;
+    return i;
+}
+
+// Birth given the position in the indiv array (useless ?)
+void birth_indiv(population* pop, int i){
+    int d = 0;  
+    while(pop->dead[d] != i) d++;
+    pop->dead[d] = pop->dead[pop->dtop--];
+    pop->alive[pop->atop++] = i;
+}
+// Birth given the position on the stack (faster) (useless ?)
+int birth_dead(population* pop, int d){
+    int i = pop->dead[d];
+    pop->dead[d] = pop->dead[pop->dtop--];
+    pop->alive[pop->atop++] = i;
+    return i;
+}
+// Birth the top of the stack (fastest) (useful)
+int birth_top(population* pop){
+    int i = pop->dead[pop->dtop--];
+    pop->alive[pop->atop++] = i;
+    return i;
+}
+
 
 void life_cycle(population* pop){
     // remove random individual => Natural death
-    int k, h;
-    sort_dead_stack(pop);
-    while(pop->alive[h] > 0){
-        k = pop->alive[h];
+    int a, i;
+    while(pop->alive[a] > 0){
+        i = pop->alive[a];
 
         // If AGING > 0 individual loose vitality at each generation
         // If vitality reach 0 they die
         if(AGING){
-            pop->indiv[k].vitality -= AGING*RAND();
+            pop->indiv[i].vitality -= AGING*RAND();
         }
-        if(pop->indiv[k].vitality < RAND()*DEATH_CONSTANT){
-            // Death
-            pop->alive[h] = -1;
-            h = INIT_POP;
-            pop->dead[pop->dtop++] = k;
+        if(pop->indiv[i].vitality < RAND()*DEATH_CONSTANT){
+            kill_alive(pop, a);
         }
     }
-    sort_alive_stack(pop);
     // Select two families
     // create a new one with crossed features
     // use Verhulst like model to conpute the number of individual of the
